5c8ab17a54e793b390ce612178f6016d3f61eb37
Viktoriia-Kapustiuk <76262397+Viktoriia-Kapustiuk@users.noreply.github.com>
2021-06-27 23:48:25 +0300

    timer works

--- index.js
+++ index.js
@@ -13,10 +13,45 @@ class Timer {
 
     start() {
 
-    this.timers = setInterval() => {
-    const currentTime = Date.now();
-    const deltaTime = this.deadline - currentTime;
-    const {days, hours, mins, secs} = this.getTime( deltaTime);
+    this.timers = setInterval(() => {
+      const currentTime = Date.now();
+      const difTime = this.deadline - currentTime;
+      const {days, hours, mins, secs} = this.getTimes(difTime);
+      setClock({days, hours,mins, secs});
+      this.stopTimer(difTime);
+    }, 100);
+}
+    getTimes(time) {
+        const days = this.pad(Math.floor(time/(1000*60*60*24))),
+              hours = this.pad(Math.floor((time % (1000*60*60*24))/(1000*60*60))),
+              mins = this.pad(Math.floor((time % (1000*60*60)) / (1000*60))),
+              secs = this.pad(Math.floor((time % (1000*60)) / 1000));
     
+        return { days, hours, mins, secs };
     }
+    
+    pad(value) {
+        return String(value).padStart(2, '0');
+    }
+    
+    stopTimer(time) {
+        if (time <= 0) {
+            clearInterval(this.timers);
+            $timer.textContent = 'Did you meet the deadline?';
+        }
+    }
+}
+
+
+const timer = new Timer({
+    onTick: setClock, selector: '#timer-1', deadline: new Date ('Jun 29, 2021'),
+});
+timer.start();
+
+function setClock ({days, hours, mins, secs}) {
+    $day.textContent = `${days}`;
+    $hour.textContent = `${hours}`;
+    $min.textContent = `${mins}`;
+    $sec.textContent = `${secs}`;
+
 }
